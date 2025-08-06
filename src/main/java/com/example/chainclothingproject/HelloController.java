6c15b78c02e8b4d280e906b0c314f2af64b185e8
shahida tisha <133828627+s77tisha@users.noreply.github.com>
2025-08-06 20:45:48 +0600

    project created

---
+++ src/main/java/com/example/chainclothingproject/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.chainclothingproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.chainclothingproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.chainclothingproject to javafx.fxml;
+    exports com.example.chainclothingproject;
+}
