9aeb66d2b9d834857647b2d82606a3d2d027829a
shahida tisha <133828627+s77tisha@users.noreply.github.com>
2025-08-07 11:23:03 +0600

    userModelClass

---
+++ src/main/java/com/example/chainclothingproject/Customer/Customer.java
@@ -0,0 +1,4 @@
+package com.example.chainclothingproject.Customer;
+
+public class Customer {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.example.chainclothingproject {
 
 
     opens com.example.chainclothingproject to javafx.fxml;
+    //opens com.example.chainclothingproject.Customer to javafx.fxml, java.base;
+    //opens com.example.chainclothingproject.DeliveryAgent to javafx.fxml, java.base;
     exports com.example.chainclothingproject;
 }
